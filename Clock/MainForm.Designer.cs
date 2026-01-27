75415c7c26b0e926d76624a308d895ef5e0a280b
Mona Dmitriy <skorobei91@mail.ru>
2026-01-27 21:26:23 +0300

    Add project files.

---
+++ Clock/MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Clock
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.labelTime = new System.Windows.Forms.Label();
+			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.checkBoxShowDate = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// labelTime
+			// 
+			this.labelTime.AutoSize = true;
+			this.labelTime.Location = new System.Drawing.Point(54, 51);
+			this.labelTime.Margin = new System.Windows.Forms.Padding(12, 0, 12, 0);
+			this.labelTime.Name = "labelTime";
+			this.labelTime.Size = new System.Drawing.Size(261, 51);
+			this.labelTime.TabIndex = 0;
+			this.labelTime.Text = "CurrentTime";
+			// 
+			// timer
+			// 
+			this.timer.Enabled = true;
+			this.timer.Tick += new System.EventHandler(this.timer_Tick);
+			// 
+			// checkBoxShowDate
+			// 
+			this.checkBoxShowDate.AutoSize = true;
+			this.checkBoxShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.checkBoxShowDate.Location = new System.Drawing.Point(63, 234);
+			this.checkBoxShowDate.Name = "checkBoxShowDate";
+			this.checkBoxShowDate.Size = new System.Drawing.Size(132, 29);
+			this.checkBoxShowDate.TabIndex = 1;
+			this.checkBoxShowDate.Text = "Show date";
+			this.checkBoxShowDate.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(25F, 51F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(806, 461);
+			this.Controls.Add(this.checkBoxShowDate);
+			this.Controls.Add(this.labelTime);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
+			this.Name = "MainForm";
+			this.Text = "ClockPV_522";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label labelTime;
+		private System.Windows.Forms.Timer timer;
+		private System.Windows.Forms.CheckBox checkBoxShowDate;
+	}
+}
+
